fa107250b01d22c3364265bcb0a8314ca08b44ae
Gage Henderson <gage@quangoinc.com>
2018-07-16 14:00:37 -0700

    Finished the basic setup

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser';
+//import PlayScene from './scenes/Play';
+
+let config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  //scene: new PlayScene(),
+  pixelArt: true
+};
+
+export default class Game {
+  constructor() {
+    // The Game
+    this.game = new Phaser.Game(config);
+  }
+}
+
+new Game();
